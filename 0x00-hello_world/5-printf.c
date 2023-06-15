58cb3d0275251c09c0b0e3e0ab087cea49f5f3ab
MentalSep <chahidjava@hotmail.com>
2023-06-15 05:52:04 -0700

    add description to file `5`

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+/**
+ * main - a program that prints a string using printf
+ *
+ * Return: always returns 0
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
