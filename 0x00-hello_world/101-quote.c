7394f5a8c4c3123ed8fa62bd94a7082ac445d957
MentalSeppuku <chahidjava@hotmail.com>
2023-06-15 05:16:25 +0100

    add task `8`

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+int main(void)
+{
+	char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	while (*str)
+	{
+		write(2, str, 1);
+		str++;
+	}
+	return (1);
+}
