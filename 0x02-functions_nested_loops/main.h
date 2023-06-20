3f03bc77c056296ea57a7a32d31f71fe2843d019
MentalSeppuku <chahidjava@hotmail.com>
2023-06-20 17:40:39 +0100

    add file of task `11`

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,6 +2,7 @@
 #define MAIN_H
 
 #include <unistd.h>
+#include <stdio.h>
 
 void print_alphabet(void);
 int _putchar(char c);
@@ -14,5 +15,6 @@ int print_last_digit(int n);
 void jack_bauer(void);
 void times_table(void);
 int add (int n, int m);
+void print_to_98(int n);
 
 #endif
