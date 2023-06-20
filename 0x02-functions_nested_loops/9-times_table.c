5e5b69c665abd3940782e1ccb34a0a18f69dcc72
MentalSeppuku <chahidjava@hotmail.com>
2023-06-20 17:24:11 +0100

    add file of task `9`

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table, starting wiht 0
+ *
+ * Return - void
+ */
+void times_table(void)
+{
+	int i, j, res;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar('0');
+		for (j = 0; j < 10; j++)
+		{
+			res = i * j;
+			if (res < 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(res + '0');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(res / 10 + '0');
+				_putchar(res % 10 + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
