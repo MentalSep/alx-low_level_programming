e6a5678e7f9a854412e5c5d55f121ee4743a3039
MentalSeppuku <chahidjava@hotmail.com>
2023-06-22 06:10:09 +0100

    add file 6 task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main"
+
+/**
+ * print_line - draws a straight line in the terminal using
+ * the character '_'
+ *
+ * @n:he number of times the character '_' should be printed
+ *
+ * Return: void
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+		_putchar('_');
+	_putchar('\n');
+}
