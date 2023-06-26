4b4add87c783fb340ce0a8b1adf918705a7f6ad2
MentalSeppuku <chahidjava@hotmail.com>
2023-06-26 07:23:39 +0100

    add file 3 task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts - returns the length of a string
+ *
+ * @str: pointer to string
+ *
+ * Return: void
+ */
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+		_putchar(str[i++]);
+	_putcha('\n');
+}
