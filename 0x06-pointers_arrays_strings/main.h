46e5928f4d332751376136977f8fea14c1a8ba2b
MentalSeppuku <chahidjava@hotmail.com>
2023-06-29 10:18:22 +0100

    fix task 10 file 102

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
@@ -11,6 +12,6 @@ char *cap_string(char *);
 char *leet(char *);
 char *rot13(char *str);
 void print_number(int n);
-int _putchar(char c);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
 
 #endif
