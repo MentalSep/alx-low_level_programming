9d88ae90ad71ec91875807a140a4f9f53a8f9c7a
MentalSeppuku <chahidjava@hotmail.com>
2023-07-03 10:21:57 +0100

    FIX: betty

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -21,7 +21,9 @@ int CheckForMatch(char c, char *str)
  * _strpbrk - searches a string for any of a set bytes.
  *
  * @s: string to check.
- * @accept: 
+ * @accept: Charatcters to match.
+ *
+ * Return: pointer to the byte in s that matches one of the bytes in accept
  */
 char *_strpbrk(char *s, char *accept)
 {
