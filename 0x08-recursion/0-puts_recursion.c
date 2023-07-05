4ac1ed35485824a4ab472549aaee6493a3fb1665
MentalSeppuku <chahidjava@hotmail.com>
2023-07-05 04:50:22 +0100

    add file 0 task 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string followed by a new line
+ *
+ * @s: string to pirnt.
+ *
+ * Return: void
+ */
+void _puts_recursion(char *s)
+{
+	if (!*s)
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	return (_puts_recursion(s + 1));
+}
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,7 @@
+#ifndef MAIN_H
+#define MAINH_H
+
+int _putchar(char c);
+void _puts_recursion(char *s);
+
+#endif
