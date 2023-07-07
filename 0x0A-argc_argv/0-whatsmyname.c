fcdadc5dd68a65d06899a2599e2217e8727ecf68
MentalSeppuku <chahidjava@hotmail.com>
2023-07-07 05:46:11 +0100

    add file 0 task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - prints the program's name, followed by a new line
+ *
+ * @ac: argument count.
+ * @av: argument vector.
+ *
+ * Return: void.
+ */
+void main(int ac, char **av)
+{
+	printf("%s\n", av[0]);
+}
