6b0e99b19e8f4c6a121a694d5d8d680538daf94b
MentalSeppuku <chahidjava@hotmail.com>
2023-07-10 10:09:17 +0100

    fix first calloc for (int *) instead of (int) file 3 task 3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -15,7 +15,7 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	grid = calloc(height, sizeof(**grid));
+	grid = calloc(height, sizeof(*grid));
 	if (!grid)
 		return (NULL);
 
