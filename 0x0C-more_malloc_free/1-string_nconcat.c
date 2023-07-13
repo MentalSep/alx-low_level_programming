f2c2e15903f4b0f139716ecf318321569c62548a
MentalSeppuku <chahidjava@hotmail.com>
2023-07-13 11:44:37 +0100

    typo in file 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -19,17 +19,17 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	for (len1 = 0; s1[len1]; len1++)
 		;
-	for (len2 = 0; s1[len2]; len2++)
+	for (len2 = 0; s2[len2]; len2++)
 		;
 
 	n >= len2 ? n = len2 : n;
-	new = malloc(sizeof(*new) * (len1 + n + 2));
+	new = malloc(sizeof(*new) * (len1 + n + 1));
 	if (!new)
 		return (NULL);
 
 	for (i = 0; s1[i]; i++)
 		new[i] = s1[i];
-	for (j = 0; j <= n; j++, i++)
+	for (j = 0; j < n; j++, i++)
 		new[i] = s2[j];
 	new[i] = '\0';
 
