a6d0b1fb52a35bc71391699158bd3fae7176b488
MentalSeppuku <chahidjava@hotmail.com>
2023-07-20 13:57:30 +0100

    case n == 0 file 0 task 0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -12,6 +12,9 @@ int sum_them_all(const unsigned int n, ...)
 	va_list args;
 	unsigned int  i, s = 0;
 
+	if (!n)
+		return (0);
+
 	va_start(args, n);
 	for (i = 0; i < n; i++)
 		s += va_arg(args, int);
