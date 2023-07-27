33289a93edd740bb1ef7f625ae206d7eea4e9da7
MentalSeppuku <chahidjava@hotmail.com>
2023-07-27 17:03:26 +0100

    add file 101 task 6

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,21 @@
+section .data
+   string: db "Hello, Holberton", 10, 0
+   format: db "%s", 0
+
+extern printf
+
+section .text
+
+   global main
+main:
+   push rbp
+
+   mov rdi, format
+   mov rsi, string
+   mov rax, 0
+   call printf
+
+   pop rbp
+
+   mov rax, 0
+   ret
