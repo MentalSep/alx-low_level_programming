bb303d50a7ea3c4ef297b924f643847516fb93a0
MentalSeppuku <chahidjava@hotmail.com>
2023-07-31 22:51:45 +0100

    add file 5 task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a listint_t list and sets the head to NULL
+ *
+ * @head: list to free
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp;
+
+	while (*head)
+	{
+		tmp = (*head)->next;
+		free((*head));
+		(*head) = tmp;
+	}
+	head = NULL;
+}
