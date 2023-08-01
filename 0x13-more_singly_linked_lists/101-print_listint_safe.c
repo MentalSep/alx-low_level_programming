074057d14315e60d1b46d1fbc7241b037d34bb84
MentalSeppuku <chahidjava@hotmail.com>
2023-08-01 20:17:19 +0100

    typo in file 101 task 12

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+
+/**
+ * print_listint_safe - prints a listint_t linked list
+ *
+ * @head: pointer to head of list
+ *
+ * Return: the number of nodes in the lsit
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	const listint_t *tmp, *holder;
+	size_t nodes;
+
+	if (!head)
+		exit(98);
+
+	tmp = head;
+	for (nodes = 0; tmp; nodes++)
+	{
+		holder = tmp;
+		tmp = tmp->next;
+		printf("[%p] %d\n", (void *)holder, holder->n);
+
+		if (holder < tmp)
+		{
+			printf("-> [%p] %d\n", (void *)tmp, tmp->n);
+			return (++nodes);
+		}
+	}
+
+	return (nodes);
+}
