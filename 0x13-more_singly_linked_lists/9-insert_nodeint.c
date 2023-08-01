63882094b5fff7b529c21d961d9bf942da196a32
MentalSeppuku <chahidjava@hotmail.com>
2023-08-01 05:52:09 +0100

    fix case idx is 0; file 9 task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -18,6 +18,14 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (!head || !new || !(*head))
 		return (NULL);
 
+	if (idx == 0)
+	{
+		new->n = n;
+		new->next = *head;
+		*head = new;
+		return (new);
+	}
+
 	tmp = *head;
 	while (tmp)
 	{
