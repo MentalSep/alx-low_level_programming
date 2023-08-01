1e5ea583efbc8e05f64e5b7c218f8aff7a2c6121
MentalSeppuku <chahidjava@hotmail.com>
2023-08-01 21:47:24 +0100

    test

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,30 +1,6 @@
 #include "lists.h"
 
 /**
- * find_loop - find the node causing the loop
- *
- * @head: head of the list
- *
- * Return: address of the node causing the loop
- */
-listint_t *find_loop(listint_t *head)
-{
-	listint_t *tmp, *end;
-
-	if (head == NULL)
-		return (NULL);
-
-	for (end = head->next; end != NULL; end = end->next)
-	{
-		if (end == end->next)
-			return (end);
-		for (tmp = head; tmp != end; tmp = tmp->next)
-			if (tmp == end->next)
-				return (end->next);
-	}
-	return (NULL);
-}
-/**
  * print_listint_safe - prints a listint_t linked list
  *
  * @head: pointer to head of list
@@ -33,22 +9,43 @@ listint_t *find_loop(listint_t *head)
  */
 size_t print_listint_safe(const listint_t *head)
 {
+	const listint_t *holder = NULL, *end, *ptr;
 	size_t nodes = 0, flag = 0;
-	listint_t *holder;
 
-	holder = find_loop((listint_t *) head);
+	if (!head)
+		return (98);
+	for (end = head->next; end; end = end->next)
+	{
+		if (end == end->next)
+		{
+			holder = end;
+			break;
+		}
+		for (ptr = head; ptr != end; ptr = ptr->next)
+		{
+			if (ptr == end->next)
+			{
+				holder = end->next;
+				break;
+			}
+		}
+		if (holder)
+			break;
+	}
 
 	while (head && (head != holder || !flag))
 	{
 		printf("[%p] %d\n", (void *)head, head->n);
 		if (head == holder)
 			flag = 1;
+
 		head = head->next;
 		nodes++;
 	}
-
 	if (holder)
-		printf("-> [%p] %d\n", (void *)head, head->n);
+		printf("-> [%p] %d\n", (void *)holder, holder->n);
 
 	return (nodes);
 }
+
+
