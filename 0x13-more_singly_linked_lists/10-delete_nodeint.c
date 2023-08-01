0d8730efa8511d654b1274e01966bf0794801fb6
MentalSeppuku <chahidjava@hotmail.com>
2023-08-01 06:37:04 +0100

    more fixing file 10 task 10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -14,7 +14,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *tmp, *holder;
 	unsigned int i = 0;
 
-	if (!head || !(*head))
+	if (!head)
 		return (-1);
 
 	tmp = *head;
