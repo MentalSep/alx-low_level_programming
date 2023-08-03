b252567171c1fb5c553c3ffe762efb60856af404
MentalSeppuku <chahidjava@hotmail.com>
2023-08-03 19:59:32 +0100

    revert to first solution file 5 task 5

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -7,15 +7,16 @@
  * @n: first number
  * @m: second number
  *
- * Return: the number of bits you would need
- * to flip to get from one number to another
+ * Return: the number of bits you would need to flip
+ * to get from one number to another
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	unsigned int count = 0;
+	unsigned int i, count = 0;
+	unsigned long int diff = n ^ m;
 
-	for (; n || m; m >>= 1, n >>= 1)
-		if ((n & 1) != (m & 1))
+	for (i = 0; i < 64; i++)
+		if ((diff >> i) & 1)
 			count++;
 
 	return (count);
