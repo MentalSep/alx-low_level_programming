9834fb17cb38528cb412a95580eba6382dd44dff
MentalSeppuku <chahidjava@hotmail.com>
2023-08-07 10:17:12 +0100

    add condition in case NULL is passed file 1 task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,9 +17,12 @@ int create_file(const char *filename, char *text_content)
 
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
+		return (-1);
+
+	if (!text_content)
 	{
 		close(fd);
-		return (-1);
+		return (1);
 	}
 
 	while (text_content[text_len])
