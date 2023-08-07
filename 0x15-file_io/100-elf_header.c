af20084d12bc5767df668e41d86d31cdc9c394af
MentalSeppuku <chahidjava@hotmail.com>
2023-08-07 23:51:38 +0100

    maybe 7

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -2,6 +2,39 @@
 #include <elf.h>
 
 /**
+ * printClass - prints class
+ *
+ * @header: Elf64_Ehdr struct
+ */
+void printClass(Elf64_Ehdr *header)
+{
+	printf("  Class:                             ");
+	if (header->e_ident[EI_CLASS] == ELFCLASSNONE)
+		printf("none\n");
+	else if (header->e_ident[EI_CLASS] == ELFCLASS32)
+		printf("ELF32\n");
+	else if (header->e_ident[EI_CLASS] == ELFCLASS64)
+		printf("ELF64\n");
+}
+
+/**
+ * printData - print type of how data is organized
+ *
+ * @header: Elf64_Ehdr struct
+ */
+void printData(Elf64_Ehdr *header)
+{
+	printf("  Data:                              ");
+
+	if (header->e_ident[EI_DATA] == ELFDATANONE)
+		printf("none\n");
+	else if (header->e_ident[EI_DATA] == ELFDATA2LSB)
+		printf("2's complement, little endian\n");
+	else if (header->e_ident[EI_DATA] == ELFDATA2MSB)
+		printf("2's complement, big endian\n");
+}
+
+/**
  * printOS - prints name of the OS
  *
  * @header: Elf64_Ehdr struct
@@ -110,16 +143,14 @@ int main(int argc, char *argv[])
 	for (i = 0; i < EI_NIDENT; i++)
 		printf(" %02x", header->e_ident[i]);
 	printf("\n");
-	printf("  Class:                             %s\n", header->e_ident[4] ==
-				ELFCLASS64 ? "ELF64" : "ELF32");
-	printf("  Data:                              %s\n", header->e_ident[5] ==
-				ELFDATA2MSB ? "2's complement, big endian" :
-				"2's complement, little endian");
+	printClass(header);
+	printData(header);
 	printf("  Version:                           %d%s",
 				header->e_ident[6], header->e_ident[6] == EV_CURRENT ?
 				" (current)\n" : "\n");
 	printOS(header);
-	printf("  ABI Version:                       %d\n", header->e_ident[8]);
+	printf("  ABI Version:                       %d\n",
+			header->e_ident[EI_ABIVERSION]);
 	printType(header);
 	printEntry(header);
 	if (close(fd) == -1)
