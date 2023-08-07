8e2975f40d36ae2370a007fe07d6ec948d750418
MentalSeppuku <chahidjava@hotmail.com>
2023-08-07 20:51:26 +0100

    betty fix file 100 task 4

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -19,7 +19,7 @@ void printOS(Elf64_Ehdr *header)
 					"UNIX - AROS", "UNIX - Fenix OS",
 					"UNIX - CloudABI"};
 
-	printf("OS/ABI:                            ");
+	printf("  OS/ABI:                            ");
 	for (i = 0; i < 18; i++)
 		if (i == header->e_ident[7])
 		{
@@ -38,7 +38,7 @@ void printOS(Elf64_Ehdr *header)
  */
 void printType(Elf64_Ehdr *header)
 {
-	printf("Type:                              ");
+	printf("  Type:                              ");
 	if (header->e_type == 1)
 		printf("REL (Relocatable file)\n");
 	else if (header->e_type == 2)
@@ -63,56 +63,40 @@ int main(int argc, char *argv[])
 	int fd, i;
 
 	if (argc != 2)
-	{
-		dprintf(STDERR_FILENO, "Usage: elf_header elf_filename\n");
-		exit(98);
-	}
+		dprintf(STDERR_FILENO, "Usage: elf_header elf_filename\n"), exit(98);
 
 	fd = open(argv[1], O_RDONLY);
 	if (fd == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: can't open file %s\n", argv[1]);
-		exit(98);
-	}
+		dprintf(STDERR_FILENO, "Error: can't open file %s\n", argv[1]), exit(98);
 
 	header = malloc(sizeof(Elf64_Ehdr));
 	if (!header)
-	{
-		dprintf(STDERR_FILENO, "Error: malloc fail\n");
-		exit(98);
-	}
-
+		dprintf(STDERR_FILENO, "Error: malloc fail\n"), exit(98);
 	if (read(fd, header, sizeof(Elf64_Ehdr)) != sizeof(Elf64_Ehdr))
-	{
-		dprintf(STDERR_FILENO, "Error: can't read from file %s\n", argv[1]);
-		exit(98);
-	}
+		dprintf(STDERR_FILENO, "Error: can't read from file %s\n",
+																	argv[1]), exit(98);
 
 	if (header->e_ident[0] != 0x7f && header->e_ident[1] != 'E' &&
 		header->e_ident[2] != 'L' && header->e_ident[3] != 'F')
-	{
-		dprintf(STDERR_FILENO, "Error: %s is not an ELF file\n", argv[1]);
-		exit(98);
-	}
+		dprintf(STDERR_FILENO, "Error: %s is not an ELF file\n", argv[1]), exit(98);
 
-	printf("Magic:  ");
+	printf("ELF Header:\n  Magic:   ");
 	for (i = 0; i < 16; i++)
-		printf(" %02x", header->e_ident[i]);
+		printf("%02x ", header->e_ident[i]);
 	printf("\n");
-
-	printf("Class:                             %s\n", header->e_ident[4] == ELFCLASS64 ? "ELF64" : "ELF32");
-	printf("Data:                              %s\n", header->e_ident[5] == ELFDATA2MSB ? "2's complement, big endian" : "2's complement, little endian");
-	printf("Version:                           %d (current)\n", header->e_ident[6]);
+	printf("  Class:                             %s\n", header->e_ident[4] ==
+																			ELFCLASS64 ? "ELF64" : "ELF32");
+	printf("  Data:                              %s\n", header->e_ident[5] ==
+																			ELFDATA2MSB ? "2's complement, big endian" :
+																			"2's complement, little endian");
+	printf("  Version:                           %d (current)\n",
+																	header->e_ident[6]);
 	printOS(header);
-	printf("ABI Version:                       %d\n", header->e_ident[8]);
+	printf("  ABI Version:                       %d\n", header->e_ident[8]);
 	printType(header);
-	printf("Entry point address:               0x%lx\n", header->e_entry);
-
+	printf("  Entry point address:               0x%lx\n", header->e_entry);
 	if (close(fd) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: can't close %s\n", argv[1]);
-		exit(98);
-	}
+		dprintf(STDERR_FILENO, "Error: can't close %s\n", argv[1]), exit(98);
 
 	return (0);
 }
