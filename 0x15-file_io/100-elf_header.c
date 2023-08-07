bdb0941c7e8ccb6f35623961426a339bc0b50ea9
MentalSeppuku <chahidjava@hotmail.com>
2023-08-07 22:12:41 +0100

    revert to maybe 2

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -73,35 +73,30 @@ int main(int argc, char *argv[])
 	header = malloc(sizeof(Elf64_Ehdr));
 	if (!header)
 		dprintf(STDERR_FILENO, "Error: malloc fail\n"), exit(98);
+
 	if (read(fd, header, sizeof(Elf64_Ehdr)) != sizeof(Elf64_Ehdr))
 		dprintf(STDERR_FILENO, "Error: can't read from file %s\n",
 																	argv[1]), exit(98);
+
 	if (header->e_ident[0] != 0x7f && header->e_ident[1] != 'E' &&
 		header->e_ident[2] != 'L' && header->e_ident[3] != 'F')
 		dprintf(STDERR_FILENO, "Error: %s is not an ELF file\n", argv[1]), exit(98);
+
 	printf("ELF Header:\n  Magic:  ");
 	for (i = 0; i < 16; i++)
 		printf(" %02x", header->e_ident[i]);
-	printf("  Class:                             %s\n",
-					header->e_ident[EI_CLASS] == ELFCLASS32 ? "ELF32" : "ELF64");
-	printf("  Data:                              %s\n",
-					header->e_ident[EI_DATA] == ELFDATA2LSB ? "2's complement, little endian" : "2's complement, big endian");
-	printf("  Version:                           %d (current)\n", header->e_ident[EI_VERSION]);
-	printf("  OS/ABI:                            %s\n",
-						header->e_ident[EI_OSABI] == ELFOSABI_SYSV ? "UNIX - System V" : "UNIX - System V");
-	printf("  ABI Version:                       %d\n", header->e_ident[EI_ABIVERSION]);
-	printf("  Type:                              %s\n",
-						header->e_type == ET_EXEC ? "EXEC (Executable file)" : "EXEC (Executable file)");
-	printf("  Entry point address:               0x%lx (bytes into file)\n", header->e_entry);
-	printf("  Start of program headers:          %ld (bytes into file)\n", header->e_phoff);
-	printf("  Start of section headers:          %ld (bytes into file)\n", header->e_shoff);
-	printf("  Flags:                             0x%x\n", header->e_flags);
-	printf("  Size of this header:               %d (bytes)\n", header->e_ehsize);
-	printf("  Size of program headers:           %d (bytes)\n", header->e_phentsize);
-	printf("  Number of program headers:         %d\n", header->e_phnum);
-	printf("  Size of section headers:           %d (bytes)\n", header->e_shentsize);
-	printf("  Number of section headers:         %d\n", header->e_shnum);
-	printf("  Section header string table index: %d\n", header->e_shstrndx);
+	printf("\n");
+	printf("  Class:                             %s\n", header->e_ident[4] ==
+																			ELFCLASS64 ? "ELF64" : "ELF32");
+	printf("  Data:                              %s\n", header->e_ident[5] ==
+																			ELFDATA2MSB ? "2's complement, big endian" :
+																			"2's complement, little endian");
+	printf("  Version:                           %d (current)\n",
+																	header->e_ident[6]);
+	printOS(header);
+	printf("  ABI Version:                       %d\n", header->e_ident[8]);
+	printType(header);
+	printf("  Entry point address:               %#lx\n", header->e_entry);
 	if (close(fd) == -1)
 		dprintf(STDERR_FILENO, "Error: can't close %s\n", argv[1]), exit(98);
 	free(header);
