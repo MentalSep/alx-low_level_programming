203528df3b02f3ff88b587b44f0bdaa2eda2c94b
MentalSep <chahidjava@hotmail.com>
2023-09-14 18:45:42 +0100

    add task 7

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index - inserts a new node at a given position
+ * @h: pointer to the head of the list
+ * @idx: index of the list where the new node should be added
+ * @n: data to insert in the new node
+ *
+ * Return: address of the new node, or NULL if it failed
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *node, *tmp;
+
+	if (!h)
+		return (NULL);
+	if (!idx)
+		return (add_dnodeint(h, n));
+	tmp = get_dnodeint_at_index(*h, idx - 1);
+	if (!tmp)
+		return (NULL);
+	if (!tmp->next)
+		return (add_dnodeint_end(h, n));
+	node = malloc(sizeof(dlistint_t));
+	if (!node)
+		return (NULL);
+	node->n = n;
+	node->prev = tmp;
+	node->next = tmp->next;
+	tmp->next->prev = node;
+	tmp->next = node;
+
+	return (node);
+}
