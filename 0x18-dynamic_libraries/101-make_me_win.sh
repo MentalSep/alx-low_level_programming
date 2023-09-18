60c921835efbb0e7f2d5334645ec03684248c291
MentalSep <chahidjava@hotmail.com>
2023-09-19 00:13:20 +0100

    add lib and adjust file 101 task 12

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P ./lib https://github.com/MentalSep/alx-low_level_programming/raw/main/0x18-dynamic_libraries/libdynamic.so
+wget -P ./lib https://raw.githubusercontent.com/MentalSep/alx-low_level_programming/main/0x18-dynamic_libraries/myCrack.so
 export LD_PRELOAD=./lib/myCrack.so
