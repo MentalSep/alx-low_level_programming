ed42601260653a466c5d7cfdf7cb1fba519476bf
MentalSep <chahidjava@hotmail.com>
2024-01-16 13:34:59 +0100

    fix taks 1

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -32,7 +32,7 @@ int binary_search(int *array, size_t size, int value)
 {
 	int left = 0, right = size - 1, mid;
 
-	if (!array)
+	if (!array || !*array)
 		return (-1);
 
 	print_array(array, 0, right);
